7de0c42edcbf687ea0c6d60e853c2ad698b6772e
Konstantinos Triantafyllou <kostas.tri@hotmail.com>
2016-09-16 16:29:54 +0300

    Add ui-router login-admin authentication

--- src/main/resources/static/js/ted.module.js
+++ src/main/resources/static/js/ted.module.js
@@ -43,29 +43,58 @@ app.config(function($httpProvider, $stateProvider, $urlRouterProvider) {
             url: '/new_item',
             templateUrl: 'partials/edit_item.html',
             controller: 'EditItemController',
-            params: { category: null }
+            params: { category: null },
+            resolve: { authenticate: authenticateUser }
         })
         .state('main.edit_item', {
             url: '/item/edit/:itemId/:itemName',
             templateUrl: 'partials/edit_item.html',
-            controller: 'EditItemController'
+            controller: 'EditItemController',
+            resolve: { authenticate: authenticateUser }
         })
         .state('main.admin', {
             url: '/admin',
             templateUrl: 'partials/admin.html',
-            controller: 'AdminController'
+            controller: 'AdminController',
+            resolve: { authenticate: authenticateAdmin }
         })
         .state('main.admin_user', {
             url: '/admin/users/:userId',
             templateUrl: 'partials/admin_user.html',
             controller: 'AdminUserController',
-            css: 'css/admin_user.css'
+            css: 'css/admin_user.css',
+            resolve: { authenticate: authenticateAdmin }
         })
         .state('page_not_found', {
             url: '/page_not_found',
             templateUrl: 'partials/page_not_found.html',
             css: 'css/page_not_found.css'
         });
+
+    function authenticateAdmin($q, $state, $timeout, AuthService) {
+        if (AuthService.isAdmin()) {
+            return $q.when()
+        } else {
+            $timeout(function() {
+                // This code runs after the authentication promise has been rejected.
+                $state.go('page_not_found')
+            });
+
+            // Reject the authentication promise to prevent the state from loading
+            return $q.reject()
+        }
+    }
+
+    function authenticateUser($q, $state, $timeout, AuthService) {
+        if (AuthService.isLoggedIn()) {
+            return $q.when()
+        } else {
+            $timeout(function() {
+                $state.go('index')
+            });
+            return $q.reject()
+        }
+    }
 });
 
 app.run(['$window', 'AuthService', function($window, AuthService) {
